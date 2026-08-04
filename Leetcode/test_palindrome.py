984fefd072f7856c7aa01b0a46986a05cee7d758
24022322-source <24022322@vnu.edu.vn>
2026-08-04 17:58:15 +0700

    Add unit-test and runtime constraint

---
+++ Leetcode/test_palindrome.py
@@ -0,0 +1,21 @@
+from validPalindrome import validPalindrome
+
+def test_chuoi_doi_xung_chuan():
+    assert validPalindrome("abccba") == True
+    assert validPalindrome("racecar") == True
+    assert validPalindrome("a") == True
+    assert validPalindrome("") == True
+
+def test_chuoi_sai():
+    assert validPalindrome("abc") == False
+    assert validPalindrome("abcd") == False
+    assert validPalindrome("abccaa") == False
+
+def test_chuoi_xoa_mot_ki_tu():
+    assert validPalindrome("abca") == True
+    assert validPalindrome("racecarx") == True
+    assert validPalindrome("aabbcc") == False
+
+def test_chuoi_rong():
+    assert validPalindrome("") == True
+
